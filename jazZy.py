d90529868d0a8095f45911151f473d2ab41c2268
Deadly-Squtch <duck-vader@hotmail.co.uk>
2019-06-12 16:05:04 +0100

    testing dancing lights

--- jazZy.py
+++ jazZy.py
@@ -1,9 +1,14 @@
 #testing phue and mqtt for motion sensing lights
+import threading
+import wave
 
+import math
 import phue
 import time
 import random
 import json
+
+import pyaudio as pyaudio
 import pygame
 
 
@@ -101,6 +106,87 @@ def run():
 		time.sleep(speed)
 		count += 1
 
+import pychorus
+from pychorus import similarity_matrix
+
+
+def test():
+	path = "./music/in_game_music_1.wav"
+	# path = "./music/in_game_music_2.wav"
+	# path = "./music/main_menu_music.wav"
+	# path = "./music/loading.wav"
+
+	mPlayer = MusicPlayer(path)
+	lPlayer = LightPlayer(path)
+
+	mPlayer.start()
+	lPlayer.run()
+
+
+class MusicPlayer(threading.Thread):
+	def __init__(s, path):
+		threading.Thread.__init__(s)
+		s.f = wave.open(path, 'r')
+		s.chunk = 1024
+		s.p = pyaudio.PyAudio()
+
+	def run(s):
+		stream = s.p.open(format=s.p.get_format_from_width(s.f.getsampwidth()),
+						channels=s.f.getnchannels(),
+						rate=s.f.getframerate(),
+						output=True)
+
+		while True:
+			data = s.f.readframes(s.chunk)
+			stream.write(data)
+			if not data:
+				break
+
+		stream.stop_stream()
+		stream.close()
+
+		s.p.terminate()
+
+
+class LightPlayer(threading.Thread):
+	def __init__(s, path):
+		threading.Thread.__init__(s)
+
+		s.b = phue.Bridge('192.168.1.211')
+		s.b.connect()
+
+		chroma, _, sr, _ = pychorus.create_chroma(path)
+		time_time_similarity = similarity_matrix.TimeTimeSimilarityMatrix(chroma, sr)
+		time_lag_similarity = similarity_matrix.TimeLagSimilarityMatrix(chroma, sr)
+
+		f = wave.open(path, 'r')
+		track_length = f.getnframes() / f.getframerate()
+
+		s.map = []
+		for i in range(int(track_length*10)):
+			slice = []
+			slice += [0.5]
+			slice += [sum(time_time_similarity.matrix[i])/len(time_time_similarity.matrix[i])]
+			slice += [sum(time_lag_similarity.matrix[i])/len(time_lag_similarity.matrix[i])]
+			s.map += [slice]
+
+
+	def run(s):
+		start = time.time()
+
+		i = 0
+		for slice in s.map:
+			command = {
+				'transitiontime': 1,
+				'bri': int(slice[0] * 254),
+				'hue': int(slice[1] * 65535),
+				'sat': int(slice[2] * 254)
+			}
+			s.b.set_light('cal', command)
+			i+=1
+			time.sleep(start + (i/10) - time.time())
+
 
 if __name__ == '__main__':
-	run()
+	# run()
+	test()
