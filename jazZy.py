5ad7a98b6c0bb77b8be4688881c2e66c565b8e64
duck-vader@hotmail.co.uk <duck-vader@hotmail.co.uk>
2019-07-04 13:25:29 +0100

    implemented a pyGame screen mode

--- jazZy.py
+++ jazZy.py
@@ -12,10 +12,8 @@
 #add TCP music stream
 #intergrate into huston
 
-
-
-
-
+import colorsys
+import sys
 import threading
 import wave
 from queue import Queue
@@ -30,6 +28,7 @@ import numpy as np
 
 import pyaudio as pyaudio
 import pygame
+from pygame.locals import *
 
 import matplotlib.pyplot as plt
 
@@ -115,59 +114,39 @@ class ButtonManager():
 			if s.button_hot[1]:
 				s.offset = (s.last-((1+s.temp["axis"][0]) / 2)) % 1
 
-			# command = (
-			# 	1-s.temp["axis"][3],
-			# 	(s.base+(((1+s.temp["axis"][0]) / 2)+s.offset)) % 1,
-			# 	1-s.temp["axis"][1]
-			# )
 		s.count += 1
 		return s.temp["axis"]
 
 
 #https://github.com/intxcc/pyaudio_portaudio/blob/master/example/echo.py
-def openStream():
-	defaultframes = 512
-
-	class textcolors:
-		blue = '\033[94m'
-		green = '\033[92m'
-		warning = '\033[93m'
-		fail = '\033[91m'
-		end = '\033[0m'
-
+def openInputStream(id=None, defaultframes=1024):
 	useloopback = False
 
-	# Use module
 	p = pyaudio.PyAudio()
 
-	# Set default to first in list or ask Windows
 	try:
 		default_device_index = p.get_default_input_device_info()
 	except IOError:
 		default_device_index = -1
 
 
-	if 1:
-		# Select Device
-		print(textcolors.blue + "Available devices:\n" + textcolors.end)
+	if id is None:
 		for i in range(0, p.get_device_count()):
 			info = p.get_device_info_by_index(i)
-			print(textcolors.green + str(info["index"]) + textcolors.end + ": \t %s \n \t %s \n" % (
-			info["name"], p.get_host_api_info_by_index(info["hostApi"])["name"]))
+			print(str(info["index"]) + ":  \t %s \t %s" % (p.get_host_api_info_by_index(info["hostApi"])["name"], info["name"]))
 
 			if default_device_index == -1:
 				default_device_index = info["index"]
 
 		# Handle no devices available
 		if default_device_index == -1:
-			print(textcolors.fail + "No device available. Quitting." + textcolors.end)
+			print("No device available. Quitting.")
 			exit()
 
 		# Get input or default
-		device_id = int(input("Choose device [" + textcolors.blue + str(
-			default_device_index) + textcolors.end + "]: ") or default_device_index)
+		device_id = int(input("Choose Input device: ") or default_device_index)
 	else:
-		device_id = 9
+		device_id = id
 	print("")
 
 	# Get device info
@@ -175,20 +154,19 @@ def openStream():
 		device_info = p.get_device_info_by_index(device_id)
 	except IOError:
 		device_info = p.get_device_info_by_index(default_device_index)
-		print(textcolors.warning + "Selection not available, using default." + textcolors.end)
+		print("Selection not available, using default.")
 
 	# Choose between loopback or standard mode
 	is_input = device_info["maxInputChannels"] > 0
 	is_wasapi = (p.get_host_api_info_by_index(device_info["hostApi"])["name"]).find("WASAPI") != -1
 	if is_input:
-		print(textcolors.blue + "Selection is input using standard mode.\n" + textcolors.end)
+		print("Selection is input using standard mode.\n")
 	else:
 		if is_wasapi:
 			useloopback = True
-			print(textcolors.green + "Selection is output. Using loopback mode.\n" + textcolors.end)
+			print("Selection is output. Using loopback mode.\n")
 		else:
-			print(
-				textcolors.fail + "Selection is output and does not support loopback mode. Quitting.\n" + textcolors.end)
+			print("Selection is output and does not support loopback mode. Quitting.\n")
 			exit()
 
 	# Open stream
@@ -203,98 +181,159 @@ def openStream():
 					input_device_index=device_info["index"],
 					as_loopback=useloopback)
 
-	return stream
+	return stream, device_info
 
+
+def openOutpoutStream(id=None, defaultframes=1024):
+	p = pyaudio.PyAudio()
+
+	try:
+		default_device_index = p.get_default_output_device_info()
+	except IOError:
+		default_device_index = -1
+
+
+	if id is None:
+		for i in range(0, p.get_device_count()):
+			info = p.get_device_info_by_index(i)
+			print(str(info["index"]) + ":  \t %s \t %s" % (p.get_host_api_info_by_index(info["hostApi"])["name"], info["name"]))
+
+			if default_device_index == -1:
+				default_device_index = info["index"]
+
+		# Handle no devices available
+		if default_device_index == -1:
+			print("No device available. Quitting.")
+			exit()
+
+		# Get input or default
+		device_id = int(input("Choose output device: ") or default_device_index)
+	else:
+		device_id = id
+	print("")
+
+	# Get device info
+	try:
+		device_info = p.get_device_info_by_index(device_id)
+	except IOError:
+		device_info = p.get_device_info_by_index(default_device_index)
+		print("Selection not available, using default.")
+
+	# Choose between loopback or standard mode
+	is_output = device_info["maxOutputChannels"] > 0
+	if device_info["maxOutputChannels"] == 0:
+		print("not an output.\n")
+		exit()
+
+	# Open stream
+	channelcount = device_info["maxInputChannels"] if (device_info["maxOutputChannels"] < device_info["maxInputChannels"]) else device_info["maxOutputChannels"]
+	stream = p.open(format=pyaudio.paInt16,
+					channels=channelcount,
+					rate=int(device_info["defaultSampleRate"]),
+					output=True,
+					frames_per_buffer=defaultframes,
+					output_device_index=device_info["index"])
+
+	return stream, device_info
+
+class LiveInputStream(threading.Thread):
+	def __init__(s, stream, chunk):
+		threading.Thread.__init__(s)
+		s.stream = stream
+		s.chunk = chunk
+		s.queue = queue.Queue()
+
+	def run(s):
+		while True:
+			s.queue.put(s.stream.read(s.chunk))
 
 class MusicPlayer(threading.Thread):
 	def __init__(s, path):
 		threading.Thread.__init__(s)
-		s.USE_WAV = True
+		s.USE_WAV = False
 		s.OUTPUT = True
 		if s.USE_WAV:
 			s.f = wave.open(path, 'r')
-			s.sampleWidth = s.f.getsampwidth()
-			s.channels = s.f.getnchannels()
-			s.framerate = s.f.getframerate()
 
 
 		s.chunk = 1024
 		s.p = pyaudio.PyAudio()
-		s.LIVE = False
-		s.JOY = True
+		s.LIVE = True
+		s.JOY = False
 		if s.LIVE:
 			# s.lf = wave.open(path, 'r')
-			s.lp = LivePlayer()
+			s.live_mapper = LiveMapper()
+			s.light_player = LightPlayer()
 			if s.JOY:
 				s.bm = ButtonManager()
 
 	def run(s):
-		if not s.USE_WAV:
-			in_stream = openStream()
+		frame_rate = 10
 
-			s.sampleWidth = 44000
-			s.framerate = 10
-
-
-		next = s.f.readframes(s.chunk)
-
-		mi = 0
-		if s.LIVE:
-			frame_rate = 10
-			sample_size = s.f.getframerate() // frame_rate
-			number_of_slices = int(s.f.getnframes() / sample_size)
-
-			li = 0
-			s.lp.start()
-			s.lp.queue.put(next)
-
-			if s.USE_WAV:
-				s.chunk = sample_size
-
+		if s.USE_WAV:
+			s.sampleWidth = s.f.getsampwidth()
+			s.channels = s.f.getnchannels()
+			s.framerate = s.f.getframerate()
+		else:
+			in_stream, input_info = openInputStream(11, s.chunk)
+			s.channels = input_info["maxOutputChannels"]
+			s.sampleWidth = s.channels
+			s.framerate = int(input_info["defaultSampleRate"])
+			s.live_input_stream = LiveInputStream(in_stream, s.chunk)
+			s.live_input_stream.start()
 
 		if s.OUTPUT:
-			out_stream = s.p.open(format=s.p.get_format_from_width(s.sampleWidth),
-				channels=s.channels,
-				rate=s.framerate,
-				output=True)
+			out_stream, output_info = openOutpoutStream(9, s.chunk)
 
+		mi = 0
 		if s.USE_WAV:
 			skip = 00
 			s.f.setpos(s.chunk * skip)
 			mi = skip
+			next = s.f.readframes(s.chunk)
+		else:
+			next = s.live_input_stream.queue.get()
+
+		if s.LIVE:
+			li = 0
+			sample_size = s.framerate // frame_rate
+			s.live_mapper.start()
+			s.light_player.start()
+
+			s.live_mapper.queue.put(next)
+
+			# if s.USE_WAV:
+			s.chunk = sample_size
 
 		while True:
 			data = next
 			if s.USE_WAV:
 				next = s.f.readframes(s.chunk)
 			else:
-				next = in_stream.read(int(sample_size/1.1))
+				next = s.live_input_stream.queue.get()
 			if s.LIVE:
 				if (mi+1)*s.chunk > li*sample_size:
-					s.lp.queue.join()
-					s.lp.light_player.send_map_slice(s.lp.slice)
-					# ldata = s.lf.readframes(sample_size)
-					ldata = next
+					s.live_mapper.queue.join()
+					s.light_player.send_map_slice(s.live_mapper.slice)
 					if s.JOY:
 						controls = np.array(s.bm.update())
 						# controls **= 2
 						controls *= 3
 						# print(controls)
-						s.lp.light_player.hueCo = 1 / (2 + controls[0])
-						s.lp.light_player.satCo = 1 / (3 + (1-controls[1]))
-						s.lp.light_player.velocity = 4.9 + (controls[2])
-						s.lp.light_player.curve = (1 + controls[3])
+						s.live_mapper.hueCo = 1 / (2 + controls[0])
+						s.live_mapper.satCo = 1 / (3 + (1-controls[1]))
+						s.live_mapper.velocity = 4.9 + (controls[2])
+						s.live_mapper.curve = (1 + controls[3])
 
 						# s.lp.light_player.hueCo = 1 / (2 + controls[0])
 						# s.lp.light_player.satCo = 1 / (3 + (1-controls[1]))
 						# s.lp.light_player.velocity = 4.9 + (controls[3])
 						# s.lp.light_player.curve = (1 + controls[4])
-					s.lp.queue.put(ldata)
+					s.live_mapper.queue.put(next)
 					li += 1
-			mdata = data
 			if s.OUTPUT:
-				out_stream.write(mdata)
-			if not mdata:
+				out_stream.write(data)
+			if not data:
 				break
 
 			if mi % 10 == 0:
@@ -308,38 +347,91 @@ class MusicPlayer(threading.Thread):
 		s.p.terminate()
 
 
-class LivePlayer(threading.Thread):
-	def __init__(s):
-		threading.Thread.__init__(s)
-		s.fr = 10
-		s.slice = (0, 0, 0)
-		s.queue = Queue()
-		s.light_player = LightPlayer()
-
-	def run(s):
-		while True:
-			data = s.queue.get()
-			if not data:
-				break
-			da = np.fromstring(data, dtype=np.int16)
-			left, right = da[0::2], da[1::2]  # left and right channel
-			sample = left
-			s.map = s.light_player.gen_slice(sample, s.fr)
-			s.queue.task_done()
-		s.queue.task_done()
-
-
 class LightPlayer(threading.Thread):
-	def __init__(s, bridgeIP='192.168.1.227', mill=False):
+	def __init__(s, bridgeIP=None, screen=True):
 		threading.Thread.__init__(s)
+		# bridgeIP = '192.168.1.227'
 		if bridgeIP is not None:
 			s.b = phue.Bridge(bridgeIP)
 			s.b.connect()
+		else:
+			s.b = None
+
+		if screen:
+			s.height = 300
+			s.width = 400
+
+			pygame.init()
+			s.display = pygame.display.set_mode((s.width, s.height))#, pygame.FULLSCREEN)
+			pygame.display.set_caption('jazZy')
+		else:
+			s.display = None
+
+		s.map = []
+
+
+	def send_map_slice(s, slice):
+		if s.b is not None:
+			command = {
+				'transitiontime': 1,
+				'hue': int(slice[0] * 65535),
+				'sat': int(slice[1] * 254),
+				'bri': int(slice[2] * 254),
+			}
+			s.b.set_light('cal', command)
+			# print(command)
+
+		if s.display is not None:
+			r, g, b = colorsys.hsv_to_rgb(*slice)
+			colour = (255*r, 255*g, 255*b)
+			s.display.fill(colour)
+			# z = np.array([[colour]*20]*20 )
+			# # z = np.array([[colour]*s.width]*s.height)
+			# # z = np.array([[colour for x in range(-s.width // 2, s.width // 2)] for y in range(-s.height // 2, s.height // 2)])
+
+			# surf = pygame.surfarray.make_surface(z)
+			# pygame.transform.smoothscale(surf, (200, s.height))
+
+			for event in pygame.event.get():
+				if event.type == QUIT:
+					pygame.quit()
+					# sys.exit()
+			# s.display.blit(surf, (0, 0))
+			pygame.display.update()
+
+	def run(s):
+		start = time.time()
+		fr = 10
+		lag = []
+
+		i = 0
+		for slice in s.map:
+			s.send_map_slice(slice)
+
+			if i % fr == 0:
+				if len(lag) == 0:
+					avg_lag = 0
+				else:
+					avg_lag = sum(lag) / len(lag)
+				print(i // fr, ": \t", avg_lag)
+				lag = []
+
+			i+=1
+			wait = start + (i/fr) - time.time()
+			if wait > 0:
+				time.sleep(wait)
+			else:
+				lag += [-wait]
+
+
+class LightMapper():
+	def __init__(s, mill=False):
+		s.center = (0.1, 0.1, 0.2)
 
 		s.velocity = 4.7  # lower is brighter
 		s.curve = 2
 		s.cap = 1.5
-		s.start = 0.0
+		s.cutin = 0.0
 		s.cutoff = 0.97
 		s.hueCo = (1 / 2)
 		s.satCo = (1 / 3)
@@ -368,6 +460,7 @@ class LightPlayer(threading.Thread):
 
 			s.requester = Requester()
 			s.requester.start()
+
 
 	def gen_slice(s, sample, frame_rate):
 		if s.mill:
@@ -401,8 +494,8 @@ class LightPlayer(threading.Thread):
 		for i in range(l):
 			freq = graph1[i]
 			theta = i / l
-			theta *= s.cutoff - s.start  # cuts pink from the rainbow
-			theta += s.start
+			theta *= s.cutoff - s.cutin  # cuts pink from the rainbow
+			theta += s.cutin
 			theta *= math.pi * 2
 			points_x += [freq * math.sin(theta)]
 			points_y += [freq * math.cos(theta)]
@@ -459,54 +552,46 @@ class LightPlayer(threading.Thread):
 			if slice_number % 100 == 0:
 				print(slice_number)
 
-		np.save('map.npy', s.map)
+		return s.map
 
-	def load(s):
-		s.map = np.load('map.npy')
 
-	def send_map_slice(s, slice):
-		# print(slice)
-
-		on = True
-		# on = (True if (map[2] > 1/254) else False)
-		command = {
-			'transitiontime': 1,
-			'hue': int(slice[0] * 65535),
-			'sat': int(slice[1] * 254),
-			'bri': int(slice[2] * 254),
-			'on': on
-		}
-		s.b.set_light('cal', command)
-		# print(command)
+class LiveMapper(threading.Thread, LightMapper):
+	def __init__(s):
+		LightMapper.__init__(s)
+		threading.Thread.__init__(s)
+		s.fr = 10
+		s.slice = (0, 0, 0)
+		s.queue = Queue()
 
 	def run(s):
-		start = time.time()
-
-		i = 0
-		for slice in s.map:
-			s.send_map_slice(slice)
-			i+=1
-			wait = start + (i/10) - time.time()
-			if wait > 0:
-				time.sleep(wait)
-
+		while True:
+			data = s.queue.get()
+			if not data:
+				break
+			da = np.fromstring(data, dtype=np.int16)
+			left, right = da[0::2], da[1::2]  # left and right channel
+			sample = right
+			s.slice = s.gen_slice(sample, s.fr)
+			s.queue.task_done()
+		s.queue.task_done()
 
 
 def test1(path):
-	lPlayer = LightPlayer(None)
-	lPlayer.generate_map(path)
+	lm = LightMapper()
+	map = lm.generate_map(path)
+	np.save('map.npy', map)
 
 	fig, ax1 = plt.subplots()
-	ax1.plot(range(len(lPlayer.map)), [bri for hue, sat, bri in lPlayer.map], color='b')
-	ax1.plot(range(len(lPlayer.map)), [hue for hue, sat, bri in lPlayer.map], color='r')
-	ax1.plot(range(len(lPlayer.map)), [sat for hue, sat, bri in lPlayer.map], color='g')
+	ax1.plot(range(len(map)), [bri for hue, sat, bri in map], color='b')
+	ax1.plot(range(len(map)), [hue for hue, sat, bri in map], color='r')
+	ax1.plot(range(len(map)), [sat for hue, sat, bri in map], color='g')
 	fig.show()
 
 
 def test2(path):
 	mPlayer = MusicPlayer(path)
 	lPlayer = LightPlayer()
-	lPlayer.load()
+	lPlayer.map = np.load('map.npy')
 
 	mPlayer.start()
 	lPlayer.run()
@@ -556,16 +641,15 @@ def test4():
 
 
 if __name__ == '__main__':
-	path = "./audio/mass.wav"
+	# path = "./audio/mass.wav"
 	# path = "./audio/kuzz.wav"	# run()
 	# path = "./audio/mozart.wav"
-	# path = "./audio/dubwise.wav"
+	path = "./audio/dubwise.wav"
 	# path = "./audio/birdy.wav"
-	test1(path)
+	# test1(path)
 	# test2(path)
-	# test3(path)
+	test3(path)
 	# test4()
-	# test5()
 
 
 
