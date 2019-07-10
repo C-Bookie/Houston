b4f5bb63517319d3bf2912a04fb4f59dba854dc1
duck-vader@hotmail.co.uk <duck-vader@hotmail.co.uk>
2019-07-10 02:06:39 +0100

    added session.py

--- main.py
+++ main.py
@@ -1,39 +1,69 @@
-
-#title: Show Host
-#aurthor: Callum B-C
+# title: Show Host
+# aurthor: Callum B-C
 
 import yaml
-import paho.mqtt.client as mqtt
+
+import connection
+import session
+
+
+class Hand(session.Node):
+	def __init__(s, conn, host=None):
+		super().__init__(conn, host)
+
+
+	def callback(s, socket_hook, data):
+		if super().callback(socket_hook, data):
+			answerSpace = connection.decode(data)
+			session.dprint(str(answerSpace))
+
+			if not s.responding:
+				raise Exception("Unexpected response/unrecognised type: " + answerSpace["type"])
+			s.session.response.put(answerSpace)
+			s.responding = False
+
+	def end(s, exception=False):  # todo review
+		s.session.end()
+
+class Show(session.Session):
+	def __init__(s, manager, name):
+		super().__init__(manager, name)
+
+
+class ShowHost(session.SessionManager):
+	def __init__(s, port=8089):
+		super().__init__(port)
+
+###
 
 def run():
-    shows = []
+	shows = []
 
-    #adding a show
-    with open("shows.yaml", 'r') as showList :
-        shows.append(Show(yaml.load(showList)["room1"]))
+	# adding a show
+	with open("shows.yaml", 'r') as showList:
+		shows.append(Show(yaml.load(showList)["room1"]))
+
 
 class Show():
-    def __init__(self, showScript):
-        self.script = showScript
+	def __init__(self, showScript):
+		self.script = showScript
+		self.reset()
 
-        self.reset()
+	# reset all IOs and states
+	def reset(self):
+		for node in self.script["panels"]:
+			for feature in self.script["panels"][node]["out"]:
+				if "default" in self.script["panels"]:
+					pass  # todo send MQTT command, topic: "show/node", finger, self.script["panels"][node][finger]["default"]
 
-    # reset all IOs and states
-    def reset(self):
-        for node in self.script["panels"]:
-            for feature in self.script["panels"][node]["out"]:
-                if "default" in self.script["panels"]:
-                    pass # todo send MQTT command, topic: "show/node", finger, self.script["panels"][node][finger]["default"]
+	# reload a shows state from an SQL database that the show host will auto save to periodically
+	def reload(self):
+		pass
 
-    # reload a shows state from an SQL database that the show host will auto save to periodically
-    def reload(self):
-        pass
-
-    # begin timer
-    def start(self):
-        pass
-
+	# begin timer
+	def start(self):
+		pass
 
 
 if __name__ == "__main__":
-    run()
+	run()
---
+++ session.py
@@ -0,0 +1,119 @@
+import threading
+import queue
+
+import connection
+
+
+DEBUG = True
+def dprint(msg):
+	if DEBUG:
+		print(str(threading.current_thread().getName()) + ": " + str(msg))
+
+
+class Node(connection.SocketHook):
+	def __init__(s, conn, host=None):
+		super().__init__(conn, host)
+		s.closeAll = threading.Event()
+		s.closeAll.clear()
+		s.session = None
+
+	def send(s, data):
+		s.send_msg(connection.encode(data))
+
+	#returns if "all clear"
+	def callback(s, socket_hook, data):
+		answerSpace = connection.decode(data)
+		dprint(str(answerSpace))
+
+		if answerSpace["type"] == "stop":
+			s.end()
+
+		elif answerSpace["type"] == "close":
+			s.close()
+
+		elif answerSpace["type"] == "trip":
+			s.end(True)
+			raise Exception("godot triped up: " + answerSpace["trip"])
+
+		elif answerSpace["type"] == "moveLobby":
+			s.session.manager.move_lobby(s, answerSpace["moveLobby"])  # todo get player ID
+
+		else:
+			return True
+		return False
+
+	def end(s, exception=False):  # todo review
+		s.session.end()
+
+	def close(s):
+		dprint("closing")
+		s.closeAll.set()
+		s.end()
+		super().close()
+
+
+class Session(threading.Thread):
+	def __init__(s, manager, name):
+		super().__init__()
+		s.manager = manager
+		s.name = name
+		s.setName("Session-" + s.name)
+		s.nodes = []
+		s.response = queue.Queue()
+
+
+	def add_node(s, node: Node):
+		s.nodes += [node]
+
+	def remove_node(s, node: Node):
+		for i in range(len(s.nodes) - 1, -1, -1):
+			if s.nodes[i] is node:
+				del s.nodes[i]
+
+	def close(s, node: Node=None):
+		s.manager.close(node)
+
+
+class SessionManager(connection.Host):
+	def __init__(s, port=8089):
+		super().__init__(port)
+		s.node_hook = Node
+		s.session_hook = Session
+		s.sessions = {}
+
+	def run(s):
+		while True:
+			conn, address = s.sock.accept()
+			assert address not in s.sessions
+			node = s.node_hook(conn, s)
+			node.address = address  # todo review
+			s.join_session(node, node.address)
+			node.start()
+
+	def move_session(s, node: Node, session: Session):
+		s.leave_session(node)
+		s.join_session(node, session.name)
+
+	def join_session(s, node: Node, des):
+		if des not in s.sessions:
+			s.sessions[des] = s.session_hook(s, des)
+			s.sessions[des].start()
+		s.sessions[des].add_node(node)
+		node.session = s.sessions[des]
+
+	def leave_session(s, node: Node):
+		node.session.remove_node()
+		if len(node.session.nodes) == 0:
+			del s.sessions[node.session.name]  # todo review
+		node.session = None
+
+	def get_sessions(s):
+		return s.sessions.keys()
+
+	def close(s, node: Node=None):  # todo rename to leave_lobby()
+		if node is not None:  # fixme deal with else
+			s.sessions[node.session].remove_node(node)
+			if len(s.sessions[node.session].nodes) == 0:
+				del s.sessions[node.session]
+
+
