eae9e10b6e99507c77d325630c02c2a7865c932c
duck-vader@hotmail.co.uk <duck-vader@hotmail.co.uk>
2019-08-11 12:19:35 +0100

    added more keys to piano

--- test_midi.py
+++ test_midi.py
@@ -21,35 +21,60 @@ def test1():
 
 
 class Piano():
-	step_up = 30
+	step_up = 0
 
 	mapping = {
-		pygame.K_q: 0,
-		pygame.K_w: 1,
-		pygame.K_e: 2,
-		pygame.K_r: 3,
-		pygame.K_t: 4,
-		pygame.K_y: 5,
-		pygame.K_u: 6,
-		pygame.K_i: 7,
-		pygame.K_o: 8,
-		pygame.K_p: 9,
-		pygame.K_a: 10,
-		pygame.K_s: 11,
-		pygame.K_d: 12,
-		pygame.K_f: 13,
-		pygame.K_g: 14,
-		pygame.K_h: 15,
-		pygame.K_j: 16,
-		pygame.K_k: 17,
-		pygame.K_l: 18,
-		pygame.K_z: 19,
-		pygame.K_x: 20,
-		pygame.K_c: 21,
-		pygame.K_v: 22,
-		pygame.K_b: 23,
-		pygame.K_n: 24,
-		pygame.K_m: 25
+		pygame.K_BACKQUOTE: 29,		# F1
+		pygame.K_1: 31,				# G1
+		pygame.K_2: 33,				# A1
+		pygame.K_3: 35,				# B1
+		pygame.K_4: 36,				# C2
+		pygame.K_5: 38,				# D2
+		pygame.K_6: 40,				# E2
+		pygame.K_7: 41,				# F2
+		pygame.K_8: 43,				# G2
+		pygame.K_9: 45,				# A2
+		pygame.K_0: 47,				# B2
+		pygame.K_MINUS: 48,			# C3
+		pygame.K_EQUALS: 50,		# D3
+
+		pygame.K_q: 43,				# G2
+		pygame.K_w: 45,				# A2
+		pygame.K_e: 47,				# B2
+		pygame.K_r: 48,				# C3
+		pygame.K_t: 50,				# D3
+		pygame.K_y: 52,				# E3
+		pygame.K_u: 53,				# F3
+		pygame.K_i: 55,				# G3
+		pygame.K_o: 57,				# A3
+		pygame.K_p: 59,				# B3
+		pygame.K_LEFTBRACKET: 60,	# C4
+		pygame.K_RIGHTBRACKET: 62,	# D4
+
+		pygame.K_a: 55,				# G3
+		pygame.K_s: 57,				# A3
+		pygame.K_d: 59,				# B3
+		pygame.K_f: 60,				# C4
+		pygame.K_g: 62,				# D4
+		pygame.K_h: 64,				# E4
+		pygame.K_j: 65,				# F4
+		pygame.K_k: 67,				# G4
+		pygame.K_l: 69,				# A4
+		pygame.K_SEMICOLON: 71,		# B4
+		pygame.K_QUOTE: 72,			# C5
+		92: 74,						# D5  HASH
+
+		60: 65,						# F4  BACKSLASH
+		pygame.K_z: 67,				# G4
+		pygame.K_x: 69,				# A4
+		pygame.K_c: 71,				# B4
+		pygame.K_v: 72,				# C5
+		pygame.K_b: 74,				# D5
+		pygame.K_n: 76,				# E5
+		pygame.K_m: 77,				# F5
+		pygame.K_COMMA: 79,			# G5
+		pygame.K_PERIOD: 81,		# A5
+		pygame.K_SLASH: 83			# B5
 	}
 
 	height = 300
@@ -71,11 +96,21 @@ class Piano():
 			events = pygame.event.get()
 			for event in events:
 				if event.type == pygame.KEYDOWN:
-					note = s.mapping[event.key] + s.step_up
-					s.send_note(note, True)
+					if event.key == pygame.K_SPACE:
+						msg = mido.Message("control_change", control=64, value=127)
+						s.port.send(msg)
+						print("Sustain Down")
+					elif event.key in s.mapping:
+						note = s.mapping[event.key] + s.step_up
+						s.send_note(note, True)
 				if event.type == pygame.KEYUP:
-					note = s.mapping[event.key] + s.step_up
-					s.send_note(note, False)
+					if event.key == pygame.K_SPACE:
+						msg = mido.Message("control_change", control=64, value=0)
+						s.port.send(msg)
+						print("Sustain Up")
+					elif event.key in s.mapping:
+						note = s.mapping[event.key] + s.step_up
+						s.send_note(note, False)
 				if event.type == pygame.QUIT:
 					return
 
