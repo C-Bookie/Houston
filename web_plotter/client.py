a1b371c6d8a5f01314b4bef656da87080742eb86
Callum B-C <duck-vader@hotmail.co.uk>
2020-08-06 02:35:59 +0100

    Implemented POST requests

--- web_plotter/client.py
+++ web_plotter/client.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
@@ -7,19 +7,32 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     with open("index.html", 'r') as file:
-        return file.read()
+        return file.read(), 200
 
 # index
 @app.route('/transponder.js')
 def get_transponder():
     with open("transponder.js", 'r') as file:
-        return file.read()
+        return file.read(), 200
 
 
 # /me
-@app.route("/me", methods=["GET"])
-def get_results():
-    return "Dummy Result"
+@app.route("/test", methods=["POST"])
+def post_test():
+    if request.method == 'POST':
+        foo = request.form.get('foo')
+        bar = int(request.form.get('bar'))
+    else:
+        foo = 'hmm'
+        bar = 0
+
+    bar *= 2
+
+    return jsonify(
+        method=request.method,
+        foo=foo,
+        bar=bar
+    ), 200
 
 
 if __name__ == "__main__":
--- web_plotter/transponder.js
+++ web_plotter/transponder.js
@@ -6,6 +6,22 @@ function httpGet(theUrl)
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
     xmlHttp.send( null );
+
+    var http = new XMLHttpRequest();
+    var url = 'test';
+    var params = 'foo=Woo&bar=3';
+    http.open('POST', url, true);
+
+    //Send the proper header information along with the request
+    http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+
+    http.onreadystatechange = function() {//Call a function when the state changes.
+        if(http.readyState == 4 && http.status == 200) {
+            alert(http.responseText);
+        }
+    }
+http.send(params);
+
     return xmlHttp.responseText;
 }
 
